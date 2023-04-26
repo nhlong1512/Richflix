05d29709fa88c98f2055fdb2a5bb44a63d958dc2
nhlong1512 <20521569@gm.uit.edu.vn>
2023-04-26 15:59:13 +0700

    Setup firebase tailwindcss and antd

---
+++ src/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAd7wbm59Yi21Q8jw4mat7xxCYG9kYPdFs",
+  authDomain: "richflix-5087c.firebaseapp.com",
+  projectId: "richflix-5087c",
+  storageBucket: "richflix-5087c.appspot.com",
+  messagingSenderId: "674401903258",
+  appId: "1:674401903258:web:6f34916f2de314e76c3523",
+};
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Space } from 'antd';
+
+const HomeScreen = () => {
+  return (
+    <div className='text-red-500 text-[30px]' >HomeScreen
+    <Button type="primary">Primary Button</Button>
+    </div>
+  )
+}
+
+export default HomeScreen
